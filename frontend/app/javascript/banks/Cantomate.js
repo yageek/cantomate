bc2ec13a14a1b1e5e83062afea94f3dd486fb8f0
Yannick Heinrich <yannick.heinrich@gmail.com>
2016-01-03 22:24:36 +0100

    Add GAE Endpoints basic loading

--- frontend/app/javascript/banks/Cantomate.js
+++ frontend/app/javascript/banks/Cantomate.js
@@ -1,6 +1,27 @@
+function startApp(){
+	window.init();
+}
+
 (function(){
-	angular.module('cantomateApp',['ngMaterial']).run(function($log){
+	angular.module('cantomateApp',['ngMaterial']).run(function($log, $window){
 		$log.debug("Starting Cantomate application...");
+
+		//Define starting application
+		$window.init = function(){
+
+			$log.debug("Initialize Cloud Endpoints...");
+			loadBankService($log);
+		};
 	});
+
+var ROOT = "http://localhost:8080/_ah/api";
+
+	function loadBankService($log){
+		$log.debug("Loading Bank services...");
+		gapi.client.load('banks', 'v1', function() {
+			$log.debug("Bank services OK");
+		}, ROOT);
+
+	};
 
 })();
